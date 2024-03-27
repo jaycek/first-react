d2ed556b5ec1ad0049ab3bdc41ede8433323e4b8
Jayce <jayce.kuruvilla@gmail.com>
2024-03-27 21:55:47 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+
+const date = new Date().toDateString()
+const img="https://picsum.photos/200/300"
+
+let greeting
+let customStyle={color:''}
+const time =new Date().getHours()
+
+if(time<12){
+    greeting="Morning"
+    customStyle.color='green'
+}
+else if(time<18){
+    greeting="Evening"
+    customStyle.color='yellow'
+
+}
+else{
+    greeting="Night"
+    customStyle.color='blue'
+
+}
+const Header = () => {
+  return (
+    <div>
+      <h1 className='head'>Today's date is {date}</h1>
+      <img src={img} alt="" />
+      <h3 style={customStyle}>Good {greeting} India!</h3>
+    </div>
+  )
+}
+
+export default Header
